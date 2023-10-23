4ca2101c1ddfc6f59478f0775325239e515db6e6
Chris Warren <chris.warren.2019@gmail.com>
2023-10-23 10:21:05 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+  const handleAddTask = () => {
+    if (task) {
+      if (editIndex !== -1) {
+        // Edit existing task
+        const updatedTasks = [...tasks];
+        updatedTasks[editIndex] = task;
+        setTasks(updatedTasks);
+        setEditIndex(-1);
+      } else {
+        // Add new task
+        setTasks([...tasks, task]);
+      }
+      setTask("");
+    }
+  };
+  const handleEditTask = (index) => {
+    const taskToEdit = tasks[index];
+    setTask(taskToEdit);
+    setEditIndex(index);
+  };
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+  const renderItem = ({ item, index }) => (
+    <View style={styles.task}>
+      <Text style={styles.itemList}>{item}</Text>
+      <View style={styles.taskButtons}>
+        <TouchableOpacity onPress={() => handleEditTask(index)}>
+          <Text style={styles.editButton}>Edit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleDeleteTask(index)}>
+          <Text style={styles.deleteButton}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Chris Warren's</Text>
+      <Text style={styles.title}>ToDo App</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter task"
+        value={task}
+        onChangeText={(text) => setTask(text)}
+      />
+      <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+        <Text style={styles.addButtonText}>
+          {editIndex !== -1 ? "Update Task" : "Add Task"}
+        </Text>
+      </TouchableOpacity>
+      <FlatList
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
